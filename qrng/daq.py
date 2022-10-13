0854614c405bf9a700b2187f2f8dab302044916e
JingYu Ning <115336923+jrunkening@users.noreply.github.com>
2022-10-13 23:35:34 +0800

    Implement `PseudoBufferedDAQ`

--- qrng/daq.py
+++ qrng/daq.py
@@ -1,4 +1,5 @@
 import abc
+from base64 import encode
 import numpy as np
 
 import Automation.BDaq as BDaq
@@ -67,10 +68,10 @@ class AbstractBufferedDAQ(abc.ABC):
         self.n_signals = 0
         self.ai_delta_v = 20.0/65536.0
 
-    def encode(self, signals: np.ndarray) -> np.ndarray:
+    def encode(self, signals: list) -> np.ndarray:
         return np.vectorize(
             lambda signal: np.uint16(np.floor((signal+10) / self.ai_delta_v))
-        )(signals)
+        )(np.array(signals))
 
     @abc.abstractmethod
     def init_device(self, n_signals: int) -> None:
@@ -106,5 +107,24 @@ class BufferedDAQ(AbstractBufferedDAQ):
         result = self.waveform_ai_ctrl.getDataF64(count=self.n_signals, timeout=-1)  # timeout=-1 meaning infinite waiting
         _, _, signals = result
 
+        return self.encode(signals)
+
     def dispose(self) -> None:
         self.waveform_ai_ctrl.dispose()
+
+
+class PseudoBufferedDAQ(AbstractBufferedDAQ):
+    def init_device(self, n_signals: int) -> None:
+        self.n_signals = n_signals
+
+    def next(self) -> np.ndarray:
+        rand_signals = np.empty(self.n_signals)
+        for i in range(len(rand_signals)):
+            rand_signals[i] = np.random.normal(loc=0.0, scale=1.0)
+            while not -10 <= rand_signals[i] <= 10:
+                rand_signals[i] = np.random.normal(loc=0.0, scale=1.0)
+
+        return self.encode(rand_signals)
+
+    def dispose(self) -> None:
+        ...
--- test/test_daq.py
+++ test/test_daq.py
@@ -20,3 +20,19 @@ def test_pseudo_instant_daq():
     assert abs(signals.sum()/len(signals) - 65536/2.0) < 10
 
     pidaq.dispose()
+
+
+def test_pseudo_buffered_daq():
+    pbdaq = daq.PseudoBufferedDAQ(DAQ_CARD)
+    pbdaq.init_device(n_signals=1048576)
+
+    assert isinstance(pbdaq.next(), np.ndarray)
+
+    signals = pbdaq.next()
+    for i in range(pbdaq.n_signals):
+        assert 0 <= signals[i] < 65536
+
+    # the mean of signals should be around the middle of 0 to 2**16
+    assert abs(signals.sum()/len(signals) - 65536/2.0) < 10
+
+    pbdaq.dispose()
